a9fecc82d823c9c1ec6c6101c46afcb22b42c6b6
Will Browar <wbrowar@gmail.com>
2022-12-14 01:04:50 -0500

    feat: minor icon tweaks

--- app.vue
+++ app.vue
@@ -8,11 +8,11 @@ import '@/assets/main.css'
     <Head>
       <Meta charset="utf-8" />
       <Meta content="width=device-width,initial-scale=1.0" name="viewport" />
-      <Meta name="theme-color" content="#1FD22C" />
+      <Meta name="theme-color" content="#010C01" />
       <Link rel="manifest" href="manifest.json" />
       <link rel="shortcut icon" href="/images/favicon.ico">
       <Link rel="icon" type="image/png" href="/images/icons-192.png" />
-      <Link color="#0054ff" href="/images/icons-vector.svg" rel="mask-icon" />
+      <Link color="#1FD22C" href="/images/icons-vector.svg" rel="mask-icon" />
     </Head>
 
     <NuxtLayout>
