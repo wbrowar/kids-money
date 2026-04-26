80baa6b11c3702e0b965689238f090817b0be5d9
Will Browar <wbrowar@gmail.com>
2026-04-26 18:11:24 -0400

    feat: changed ping responst to show origin

--- server/src/api/ping.ts
+++ server/src/api/ping.ts
@@ -2,6 +2,6 @@ import { Request, Response } from 'express'
 import { DbMessage } from '@/utils/db-response.ts'
 
 export async function ping(req: Request, res: Response) {
-  const response = new DbMessage(`pong ${req.get('referrer') ?? 'referrer not set'}`)
+  const response = new DbMessage(`pong ${req.get('origin') ?? 'referrer not set'}`)
   res.send(JSON.stringify(response))
 }
