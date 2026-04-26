b0696f2849cd82eb284467b7ded13aae6dd35e6d
Will Browar <wbrowar@gmail.com>
2026-04-26 11:21:16 -0400

    feat: changed start script to use specific node version

--- server/scripts/start.sh
+++ server/scripts/start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 . ~/.nvm/nvm.sh      # Load NVM
-nvm use              # Switch to specific version
+nvm use 24           # Switch to specific version
 node src/server.ts   # Run your application
