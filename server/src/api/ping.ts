68b2de88d0c432c3f62f2b65449cdd6045abb0ea
Will Browar <wbrowar@gmail.com>
2026-04-26 19:07:51 -0400

    feat: logging requests for ping

--- server/src/api/ping.ts
+++ server/src/api/ping.ts
@@ -2,6 +2,8 @@ import { Request, Response } from 'express'
 import { DbMessage } from '@/utils/db-response.ts'
 
 export async function ping(req: Request, res: Response) {
-  const response = new DbMessage(`pong ${req.get('origin') ?? 'referrer not set'}`)
+  const message = `pong ${req.get('origin') ?? 'referrer not set'}`
+  console.log(message)
+  const response = new DbMessage(message)
   res.status(200).send(JSON.stringify(response))
 }
